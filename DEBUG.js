e077c0a5d3b37c2d951e9e1791c45893a07d6c8e
TermTanti <termsakt@gmail.com>
2017-07-18 14:34:41 +0700

    Fix url

--- DEBUG.js
+++ DEBUG.js
@@ -1106,7 +1106,7 @@
   var descriptor = {
     blocks: blocks[lang],
     menus: menus[lang],
-    url: 'http://khanning.github.io/scratch-arduino-extension'
+    url:'http://TermTanti.github.io/Arduino_ScratchX_TEST'
   };
 
   ScratchExtensions.register('Arduino', descriptor, ext, {type:'serial'});
